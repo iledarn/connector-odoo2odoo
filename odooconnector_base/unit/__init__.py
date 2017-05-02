86d4231aae33fce25dcf710136c465ded57ecd65
iledarn <nasyrov@it-projects.info>
2017-05-02 16:26:18 +0500

    [REF] odooconnector_base: use new oca connector module

--- odooconnector_base/unit/__init__.py
+++ odooconnector_base/unit/__init__.py
@@ -3,6 +3,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import backend_adapter
 from . import binder
-from . import export_synchronizer
 from . import import_synchronizer
 from . import mapper
